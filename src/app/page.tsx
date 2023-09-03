b2a0037a8e26036a61b930b47e00901ffe97469e
Tejas Mayekar <tejasmayekar08@gmail.com>
2023-09-03 19:49:29 +0530

    meta desc update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
       "@type": "WebSite",
       "name": "STOFFSPOT",
       "url": "https://stoffspot.com/",
-      "description": "Discover a world of style at StoffSpot – your premier destination for buying and selling quality clothing online. Explore a diverse marketplace filled with fashion treasures, where you can effortlessly refresh your wardrobe or turn your gently worn garments into cash. Join our community of fashion enthusiasts today!",
+      "description": "StoffSpot is a top online marketplace for buying and selling high-quality clothing, offering a diverse range of options to refresh your wardrobe or turn gently worn garments into cash.",
       "publisher": {
         "@type": "Organization",
         "name": "STOFFSPOT",
