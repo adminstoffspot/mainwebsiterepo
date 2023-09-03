1b8c846b58ca10f238b0f689c364c92deeed28d8
FIRST_NAME LAST_NAME <dsmanishsingh@gmail.com>
2023-09-03 19:29:34 +0530

    Manish

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+
+const nextConfig = {
+    reactStrictMode: true,
+    async rewrites() {
+        return [
+            {
+                source: '/robots.txt',
+                destination: '/api/robots'
+            }
+        ];
+    }
+  }
 
 module.exports = nextConfig
