acce5233daed139b693f0e4031ebc644f406858a
FIRST_NAME LAST_NAME <dsmanishsingh@gmail.com>
2023-09-03 21:00:36 +0530

    Manish

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,10 +14,46 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
+  const addJsonLd = () => {
+    return {
+      __html: `
+      "@context": "http://schema.org",
+      "@type": "WebSite",
+      "name": "STOFFSPOT",
+      "url": "https://stoffspot.com/",
+      "description": "StoffSpot is a online marketplace for buying and selling clothing, offering a diverse range of options to refresh your wardrobe or turn worn garments into cash.",
+      "publisher": {
+        "@type": "Organization",
+        "name": "STOFFSPOT",
+        "logo": {
+          "@type": "ImageObject",
+          "url": 'https://stoffspot.com/Assets/lOGO-02.png',
+          "width": 200,
+          "height": 50
+        }
+      }
+      `
+    }
+  }
   return (
     <html lang="en">
       <head>
         <link rel='icon' href='https://stoffspot.com/Assets/lOGO-01.png'/>
+        <meta
+          property="og:description"
+          content="StoffSpot is a online marketplace for buying and selling clothing"
+        />
+        <meta
+          property="og:image"
+          content="./Assets/lOGO-02.png"
+        />
+        <meta name="google-site-verification" content="9V1wd7g77Y9gGLlgJlia3CCJ2qI8DOFholXrXgB6Ilw" />
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={addJsonLd()}
+          key="product-jsonld"
+        />
+        <link rel="canonical" href="https://stoffspot.com/" />
       </head>
       <body className={inter.className}>{children}</body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,51 +1,11 @@
 import Navbar from './Components/Navbar'
-import Head from 'next/head';
 import { FiFacebook } from 'react-icons/fi';
 import { FiTwitter } from 'react-icons/fi';
 import { FiInstagram } from 'react-icons/fi';
 import { FiLinkedin } from 'react-icons/fi';
 export default function Home() {
-  const addJsonLd = () => {
-    return {
-      __html: `
-      "@context": "http://schema.org",
-      "@type": "WebSite",
-      "name": "STOFFSPOT",
-      "url": "https://stoffspot.com/",
-      "description": "StoffSpot is a online marketplace for buying and selling clothing, offering a diverse range of options to refresh your wardrobe or turn worn garments into cash.",
-      "publisher": {
-        "@type": "Organization",
-        "name": "STOFFSPOT",
-        "logo": {
-          "@type": "ImageObject",
-          "url": 'https://stoffspot.com/Assets/lOGO-02.png',
-          "width": 200,
-          "height": 50
-        }
-      }
-      `
-    }
-  }
   return (
-
     <>
-      <Head>
-      <meta name="google-site-verification" content="9V1wd7g77Y9gGLlgJlia3CCJ2qI8DOFholXrXgB6Ilw" />
-        <script
-          type="application/ld+json"
-          dangerouslySetInnerHTML={addJsonLd()}
-          key="product-jsonld"
-        />
-        <meta
-          property="og:description"
-          content="StoffSpot is a online marketplace for buying and selling clothing"
-        />
-        <meta
-          property="og:image"
-          content="./Assets/lOGO-02.png"
-        />
-        <link rel="canonical" href="https://stoffspot.com/" />
-      </Head>
       <Navbar />
       <div className="h-screen w-screen bg-no-repeat bg-cover bg-fixed grid justify-center items-center text-6xl CommingSoon">
         <div className='text-center flex flex-col items-center text-white'>
