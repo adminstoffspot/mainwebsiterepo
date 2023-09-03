2bab74afcfb002f5f1d64f9d4d394d329cd5bb1a
FIRST_NAME LAST_NAME <dsmanishsingh@gmail.com>
2023-09-03 21:15:56 +0530

    Manish

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,21 +16,39 @@ export default function RootLayout({
 }) {
   const addJsonLd = () => {
     return {
-      __html: `
-      "@context": "http://schema.org",
-      "@type": "WebSite",
-      "name": "STOFFSPOT",
-      "url": "https://stoffspot.com/",
-      "description": "StoffSpot is a online marketplace for buying and selling clothing, offering a diverse range of options to refresh your wardrobe or turn worn garments into cash.",
-      "publisher": {
-        "@type": "Organization",
-        "name": "STOFFSPOT",
-        "logo": {
-          "@type": "ImageObject",
-          "url": 'https://stoffspot.com/Assets/lOGO-02.png',
-          "width": 200,
-          "height": 50
-        }
+      __html: `{
+      "@context": "https://schema.org",
+  "@type": "ShoppingCenter",
+  "name": "Stoffspot Private Limited",
+  "image": "https://stoffspot.com/Assets/lOGO-01.png",
+  "@id": "",
+  "url": "https://stoffspot.com/",
+  "telephone": "",
+  "address": {
+    "@type": "PostalAddress",
+    "streetAddress": "Nr Hanuman Nagar Bhandup",
+    "addressLocality": "Mumbai",
+    "postalCode": "400078",
+    "addressCountry": "IN"
+  },
+  "openingHoursSpecification": {
+    "@type": "OpeningHoursSpecification",
+    "dayOfWeek": [
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+      "Sunday"
+    ],
+    "opens": "00:00",
+    "closes": "23:59"
+  },
+  "sameAs": [
+    "https://www.linkedin.com/company/stoffspot-private-limited/",
+    "https://www.instagram.com/stoff_spot/?igshid=MWZjMTM2ODFkZg%3D%3D"
+  ] 
       }
       `
     }
@@ -38,7 +56,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel='icon' href='https://stoffspot.com/Assets/lOGO-01.png'/>
+        <link rel='icon' href='https://stoffspot.com/Assets/lOGO-01.png' />
         <meta
           property="og:description"
           content="StoffSpot is a online marketplace for buying and selling clothing"
