a7de5acbb9d73dd032e009eca8b6ff2ba349c975
FIRST_NAME LAST_NAME <dsmanishsingh@gmail.com>
2023-09-03 20:26:33 +0530

    manish

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
 import Navbar from './Components/Navbar'
 import Head from 'next/head';
+import { FiFacebook } from 'react-icons/fi';
+import { FiTwitter } from 'react-icons/fi';
+import { FiInstagram } from 'react-icons/fi';
+import { FiLinkedin } from 'react-icons/fi';
 export default function Home() {
   const addJsonLd = () => {
     return {
@@ -37,6 +41,12 @@ export default function Home() {
         <div className='text-center flex flex-col items-center text-white'>
           <div className='h-30'><img src='./Assets/lOGO-02.png' alt='hello' className='w-96 h-max' /></div>
           <div className='font-bold'>COMING SOON</div>
+          <div className="flex mt-2">
+          <a href="/" className="twitter text-2xl mx-3"><FiTwitter/></a>
+          <a href="/" className="facebook text-2xl mx-3"><FiFacebook/></a>
+          <a href="https://www.instagram.com/stoff_spot/?igshid=MWZjMTM2ODFkZg==" className="instagram text-2xl mx-3"><FiInstagram/></a>
+          <a href="https://www.linkedin.com/company/stoffspot-private-limited/" className="linkedin text-2xl mx-3"><FiLinkedin/></a>
+        </div>
         </div>
       </div>
     </>
