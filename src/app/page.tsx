de2ce37fd37ec25656d2908e45b469ab3b7e6879
FIRST_NAME LAST_NAME <dsmanishsingh@gmail.com>
2023-09-04 15:13:47 +0530

    manish

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
           <h2 className='hidden'>Women,stoffspot soon comming,CLOTHINGS, FASHION,RECYCLE,FEMINISM,MENSWEAR,MEN, KIDS,KIDS WEAR,earning,login,basket,open</h2>
           <div className="flex mt-2">
           <a href="/" className="twitter text-2xl mx-3"><FiTwitter/></a>
-          {/* <a href="/" className="facebook text-2xl mx-3"><FiFacebook/></a> */}
+          <a href="/" className="facebook text-2xl mx-3"><FiFacebook/></a>
           <a href="https://www.instagram.com/stoff_spot/?igshid=MWZjMTM2ODFkZg==" className="instagram text-2xl mx-3"><FiInstagram/></a>
           <a href="https://www.linkedin.com/company/stoffspot-private-limited/" className="linkedin text-2xl mx-3"><FiLinkedin/></a>
         </div>
