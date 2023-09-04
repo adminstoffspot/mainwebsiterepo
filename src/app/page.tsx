5c06c157b2d227002c60b7f3d94b4511431975f0
FIRST_NAME LAST_NAME <dsmanishsingh@gmail.com>
2023-09-04 16:09:09 +0530

    manish

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
       <div className="h-screen w-screen bg-no-repeat bg-cover bg-fixed grid justify-center items-center text-6xl CommingSoon">
         <div className='text-center flex flex-col items-center text-white'>
           <div className='h-30 flex flex-1 justify-center'>
-            <img src='./Assets/lOGO-02.webp' height={'400'} width={'400'} alt='hello' className='h-auto max-w-md justify-center' />
+            <img src='./Assets/lOGO-02.webp' height={'400'} width={'400'} alt='hello' className=' justify-center' />
             </div>
           <div className='font-bold md:text-6xl text-2xl'>COMING SOON</div>
           <h1 className='hidden'>STOFFSPOT</h1>
